a0cc5f4449f9a8792af1b2af47bdf3f1e471eeaf
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2023-02-09 15:37:04 -0800

    Add limelight functions

--- src/main/cpp/subsystems/Limelight/Limelight.cpp
+++ src/main/cpp/subsystems/Limelight/Limelight.cpp
@@ -5,7 +5,7 @@
 
 abval::Limelight* abval::Limelight::instance = nullptr;
 
-//
+// private funcs
 
 abval::Limelight::Limelight(){}
 
@@ -20,6 +20,12 @@ abval::Limelight* abval::Limelight::GetInstance(){
 
 //
 
+std::shared_ptr<nt::NetworkTable> abval::Limelight::getTable(){
+  return nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+}
+
+// public funcs
+
 void abval::Limelight::setupPortForwarding(){
   fmt::print("test");
 
@@ -30,4 +36,100 @@ void abval::Limelight::setupPortForwarding(){
   fmt::print("test");
 }
 
+// Basic Targeting Data
 
+bool abval::Limelight::getValidTargetDetected(){ // tv
+  return getTable()->GetBoolean("tv", false);
+}
+
+double abval::Limelight::getTargetHorizontalOffset(){ // tx
+  return getTable()->GetNumber("tx", 0.0);
+}
+
+double abval::Limelight::getTargetVerticalOffset(){ // ty
+  return getTable()->GetNumber("ty", 0.0);
+}
+
+double abval::Limelight::getTargetArea(){ // ta
+  return getTable()->GetNumber("ta", 0.0);
+}
+
+double abval::Limelight::getPipelineLatency(){ // tl
+  return getTable()->GetNumber("tl", 0.0);
+}
+
+double abval::Limelight::getBoundingBoxShort(){ // tshort 
+  return getTable()->GetNumber("tshort", 0.0);
+}
+
+double abval::Limelight::getBoundingBoxLong(){ // tlong
+  return getTable()->GetNumber("tlong", 0.0);
+}
+
+double abval::Limelight::getRoughBoundingHorizontal(){ // thor
+  return getTable()->GetNumber("thor", 0.0);
+}
+
+double abval::Limelight::getRoughBoundingVertical(){ // tvert
+  return getTable()->GetNumber("tvert", 0.0);
+}
+
+double abval::Limelight::getActivePipelineIndex(){ // getpipe
+  return getTable()->GetNumber("getpipe", 0.0);
+}
+
+double abval::Limelight::getTargetClassID(){ // tclass
+  return getTable()->GetNumber("tclass", 0.0);
+}
+
+// April Tag and 3D data
+
+std::vector<double> abval::Limelight::getBotPose(){
+  return getTable()->GetNumberArray("botpose", std::vector<double>(6));
+}
+
+std::vector<double> abval::Limelight::getBotPose_wpiblue(){
+  return getTable()->GetNumberArray("botpose_wpiblue", std::vector<double>(6));
+}
+
+std::vector<double> abval::Limelight::getBotPose_wpired(){
+  return getTable()->GetNumberArray("botpose_wpired", std::vector<double>(6));
+}
+
+std::vector<double> abval::Limelight::getCameraPose_targetspace(){
+  return getTable()->GetNumberArray("camerapose_targetspace", std::vector<double>(6));
+}
+
+std::vector<double> abval::Limelight::getTargetPose_cameraspace(){
+  return getTable()->GetNumberArray("targetpose_cameraspace", std::vector<double>(6));
+}
+
+std::vector<double> abval::Limelight::getTargetPose_robotspace(){
+  return getTable()->GetNumberArray("targetpose_robotspace", std::vector<double>(6));
+}
+
+std::vector<double> abval::Limelight::getBotPose_targetspace(){
+  return getTable()->GetNumberArray("botpose_targetspace", std::vector<double>(6));
+}
+
+double abval::Limelight::getPrimaryAprilTagID(){
+  return getTable()->GetNumber("tid", 0.0);
+}
+
+// Camera controls, reference network tables api for options
+
+void abval::Limelight::setLEDMode(int mode){ 
+  getTable()->PutNumber("ledMode", mode);
+}
+
+void abval::Limelight::setCameraMode(int mode){
+  getTable()->PutNumber("camMode", mode);
+}
+
+void abval::Limelight::setPipeline(int index){
+  getTable()->PutNumber("pipeline", index);
+}
+
+void abval::Limelight::setStreamMode(int mode){
+  getTable()->PutNumber("stream", mode);
+}
--- src/main/include/subsystems/Limelight/Limelight.h
+++ src/main/include/subsystems/Limelight/Limelight.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <vector>
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+
 namespace abval{
     class Limelight {  
     private:
@@ -9,9 +13,56 @@ namespace abval{
 
         Limelight();
         ~Limelight();
+
+        //
+
+        std::shared_ptr<nt::NetworkTable> getTable();
     public:
         static Limelight* GetInstance();
 
         void setupPortForwarding();
+
+        // Basic Targeting Data
+
+        bool getValidTargetDetected(); // tv
+        double getTargetHorizontalOffset(); // tx
+        double getTargetVerticalOffset(); // ty
+        double getTargetArea(); // ta
+
+        double getPipelineLatency(); // tl
+
+        double getBoundingBoxShort(); // tshort
+        double getBoundingBoxLong(); // tlong
+
+        double getRoughBoundingHorizontal(); // thor
+        double getRoughBoundingVertical(); // tvert
+
+        double getActivePipelineIndex(); // getpipe
+
+        double getTargetClassID(); // tclass
+
+        // April Tag and 3D data
+
+        std::vector<double> getBotPose();
+        std::vector<double> getBotPose_wpiblue();
+        std::vector<double> getBotPose_wpired();
+
+        std::vector<double> getCameraPose_targetspace();
+        std::vector<double> getTargetPose_cameraspace();
+
+        std::vector<double> getTargetPose_robotspace();
+        std::vector<double> getBotPose_targetspace();
+
+        double getPrimaryAprilTagID();
+
+        //
+
+        void setLEDMode(int mode);
+        void setCameraMode(int mode);
+        void setPipeline(int index);
+        void setStreamMode(int mode);
+
+        // void takeSnapshot(); too lazy to implement
+
     };
 }  // namespace abval
