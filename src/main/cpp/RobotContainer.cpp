5f0eb44f5e49bad59879c7ded6fa24d78cb25412
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2023-02-18 20:21:27 -0800

    Fix unilateral turning bug; Document

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -31,36 +31,59 @@ RobotContainer::RobotContainer()
       swerve() {
   ConfigureBindings();
 }
+
 void RobotContainer::ConfigureBindings() {
   using namespace units;
+  
   swerve.SetDefaultCommand(frc2::RunCommand(
       [this]() {
+          
+        auto heading = 90_deg; // TODO: replace with IMU reading
+          
+        // Convert x, y, and angular velocities from field to chassis perspective.
         auto speed = frc::ChassisSpeeds::FromFieldRelativeSpeeds(
-            frc::ChassisSpeeds{.vx = -meters_per_second_t(joystick.GetX()),
-                               .vy = meters_per_second_t(joystick.GetY()),
-                               .omega = radians_per_second_t(joystick.GetZ())},
-            90_deg);
-        if (units::math::hypot(speed.vx, speed.vy) > .1_mps ||
-            speed.omega > .1_rad_per_s)
-          swerve.setSpeed(speed);
-        else
+            frc::ChassisSpeeds{
+                .vx = -meters_per_second_t(joystick.GetX()),
+                .vy = meters_per_second_t(joystick.GetY()),
+                .omega = radians_per_second_t(joystick.GetZ())
+            }, heading
+        );
+          
+        // If speed is below threshold, don't take it
+        auto min_linear_speed = 0.1_mps;
+        auto min_angular_speed = 0.1_rad_per_s;
+        if (units::math::hypot(speed.vx, speed.vy) < min_linear_speed &&
+            units::math::fabs(speed.omega) < min_angular_speed)
+        {
           swerve.setSpeed(frc::ChassisSpeeds{});
+          return;
+        }
+        
+        swerve.setSpeed(speed);
       },
       {&swerve}));
 
-  static bool b = false;
+  static bool turn_pid_enabled = false;
+    
+  // Toggle pod turn PID
   joystick.Button(3).OnTrue(
       frc2::InstantCommand(
           [&, this]() {
-            swerve.enablePID(b);
-            b = !b;
+            swerve.enableTurnPID(turn_pid_enabled);
+            turn_pid_enabled = !turn_pid_enabled;
           },
           {&swerve})
           .ToPtr());
+  
+  // Zero pod encoders
   joystick.Button(4).OnTrue(
       frc2::InstantCommand([this]() { swerve.zero(); }, {&swerve}).ToPtr());
+  
+  // Return pods to 0 rotation
   joystick.Button(7).OnTrue(
       frc2::InstantCommand([this]() { swerve.home(); }, {&swerve}).ToPtr());
+    
+  // To drive or not to drive
   joystick.Button(11)
       .OnTrue(
           frc2::RunCommand([this]() { swerve.setPower(0.1); }, {&swerve})
