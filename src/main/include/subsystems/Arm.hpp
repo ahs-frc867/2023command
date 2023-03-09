1dccf1fbfb68e243a0eacb0dd33ea51b718ca371
Shin Umeda <umeda.shin@gmail.com>
2023-03-08 16:21:35 -0800

    changed feedforward to builtin class, converted encoder to radian

--- src/main/include/subsystems/Arm.hpp
+++ src/main/include/subsystems/Arm.hpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <ctre/phoenix/motorcontrol/can/TalonSRX.h>
 #include <frc/DoubleSolenoid.h>
+#include <frc/controller/ArmFeedforward.h>
 #include <frc2/command/PIDCommand.h>
 #include <frc2/command/SubsystemBase.h>
 #include <numbers>
@@ -16,24 +17,35 @@ namespace abval {
 using ctre::phoenix::motorcontrol::can::TalonSRX;
 using units::radian_t;
 class Arm : public frc2::SubsystemBase {
+  typedef units::unit_t<frc::ArmFeedforward::ka_unit> kA_t;
+  typedef units::unit_t<frc::ArmFeedforward::kv_unit> kV_t;
+  typedef units::unit_t<frc::ArmFeedforward::Velocity> v_t;
   TalonSRX motor;
   frc::Encoder encoder;
   frc2::PIDController pid;
   frc::DoubleSolenoid collector;
-  double kS = 0, kV = 0;
+  frc::ArmFeedforward feedforward;
+  double kS = 0, kV = 0, kG = 0;
+  double prev_s;
   void Periodic() override {
     using ctre::phoenix::motorcontrol::ControlMode;
     auto distance = encoder.GetDistance();
-    auto err = pid.GetSetpoint() - distance;
-    motor.Set(ControlMode::PercentOutput,
-              pid.Calculate(distance) + kV * err + std::copysign(kS, err));
+    v_t v = (radian_t(distance) - radian_t(prev_s)) / 20_ms;
+    prev_s = distance;
+    motor.Set(
+        ControlMode::PercentOutput,
+        (pid.Calculate(distance) +
+         feedforward.Calculate(radian_t(encoder.GetDistance()), v).value()) /
+            (12_V).value());
   }
 
 public:
   Arm(int motor_id, int chan_a, int chan_b)
       : motor(motor_id), encoder(chan_a, chan_b, false),
-        collector(frc::PneumaticsModuleType::CTREPCM, 0, 1), pid(1, 0, 0) {
+        collector(frc::PneumaticsModuleType::CTREPCM, 0, 1), pid(1, 0, 0),
+        feedforward(0_V, 0_V, kV_t(0), kA_t(0)) {
     pid.EnableContinuousInput(0, std::numbers::pi * 2);
+    encoder.SetDistancePerPulse(2 * std::numbers::pi / 600);
   }
   void setTarget(radian_t target) noexcept { pid.SetSetpoint(target.value()); }
 };
